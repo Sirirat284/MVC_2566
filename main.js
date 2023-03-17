2e2687ced22149a41daed2ea77c42c7c4f4d299e
Dilldo <dodo0989263290@gmail.com>
2023-03-17 22:27:31 +0700

    connect foder view

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const app = express();
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser');
 
-
-
+app.set('view engine', 'ejs')
+app.set('views',path.join(__dirname, 'view'))
 
 // For Master process
 if (cluster.isMaster) {
